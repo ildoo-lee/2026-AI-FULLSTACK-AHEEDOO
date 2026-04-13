d637b4a5cd8e9a5b15467f3be9cd3fa5c3a675a0
ildoo-lee <aheedoo@kakao.com>
2026-04-14 01:41:18 +0900

    arr

--- track003_java/java2026/src/com/the703/days/Day014_lid.java
+++ track003_java/java2026/src/com/the703/days/Day014_lid.java
@@ -1,10 +1,48 @@
 package com.the703.days;
 
+import java.util.Scanner;
+
 public class Day014_lid {
 
 	public static void main(String[] args) {
-		// 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자 입력(1,2,3) > ");
+		int day = scanner.nextInt();
+		
+			 
+		
+		// 1. if 버전
+			      if (day == 1) {System.out.println("1이다");} 
+			 else if (day == 2) {System.out.println("2이다");} 
+			 else if (day == 3) {System.out.println("3이다");} 
+			 else               {System.out.println("1,2,3이 아니다");}
+        
+		     
+			      
+			      
+			 
+			      
+			      
+	    // 2. switch 버전
+		     
+		     
+			 
+			      
+			      
+	    // 3. for, while, do while   1 2 3 4
+		
 
-	}
+	    // 4. 이중 for ★ a <= x ? '#' : ""
+		     
+	    // 5. 배열
+		     int[] arr2 = {1,2,3};
+		     
+	    // 6. 1차원배열
+		     
+		     
+		     
+		     
+		     
+	} // main
 
-}
+} // class
