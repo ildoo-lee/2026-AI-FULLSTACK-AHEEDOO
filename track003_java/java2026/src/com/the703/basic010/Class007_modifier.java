43f47484e417d2a1d8ba761e3653f1b3daba18b5
ildoo-lee <aheedoo@kakao.com>
2026-04-24 01:40:38 +0900

    move

--- track003_java/java2026/src/com/the703/basic010/Class007_modifier.java
+++ track003_java/java2026/src/com/the703/basic010/Class007_modifier.java
@@ -4,7 +4,7 @@ package com.the703.basic010;
 class UserInfo{
 	public    String name;     // 아무데서나 다 접근가능
 	protected String safeCode; // 자식에서 사용가능 (extends 시)
-              String house;    // 피키지(폴더)
+  /*default*/ String house;    // 피키지(폴더)
     private   int    iQ;       // 클래스 내부 (아버지의  IQ는 아버지만)
     
     //alt+shift+s : Generate Getters and Setters iQ 사용할 수 있도록 해줌
